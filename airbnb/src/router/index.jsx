01b70bd361e62e62aeb57b1f2a0412ab940b76be
ToanVT1 <166732616+ToanVT1@users.noreply.github.com>
2024-10-01 20:39:55 +0700

    feature_homepage_fix_2

--- airbnb/src/router/index.jsx
+++ airbnb/src/router/index.jsx
@@ -5,7 +5,9 @@ import Register from "../atomic/pages/register/index";
 import Login from "../atomic/pages/login/index";
 import { UserTemplate } from "../atomic/templates/user/user.template";
 
+
 export const router = createBrowserRouter([
+ 
   {
     path: "/",
     element: <UserTemplate />,
